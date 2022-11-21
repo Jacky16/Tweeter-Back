0dfa7a34271beaecf2570c9443b326fd2cecfb1c
Adria <adriamartinez94@gmail.com>
2022-11-21 19:13:54 +0100

    Init Server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import environment from "./loadEnvironments.js";
+import app from "./app.js";
+import startServer from "./server/index.js";
 
+const { port } = environment;
+
+await startServer(Number(port), app);
