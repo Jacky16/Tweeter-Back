861d999f8bfe85fa993a63cd89b5be73c94b52ec
Adria <adriamartinez94@gmail.com>
2022-11-21 20:59:37 +0100

    Add general error with test

--- src/server/middleware/errors/errors.test.ts
+++ src/server/middleware/errors/errors.test.ts
@@ -1,6 +1,7 @@
 import type { Response } from "express";
+import CustomError from "../../../CustomError/CustomError.js";
 import errorsMessage from "../../../errorsMessage";
-import { endpointNotFound } from "./error.js";
+import { endpointNotFound, generalError } from "./error.js";
 
 describe("Given the middleware endpointNotFound", () => {
   describe("When it receives a unknown endpoint", () => {
@@ -17,6 +18,47 @@ describe("Given the middleware endpointNotFound", () => {
       endpointNotFound(null, res as Response, null);
 
       expect(res.json).toBeCalledWith(expectedMessage);
+      expect(res.status).toBeCalledWith(expectedStatus);
+    });
+  });
+});
+describe("Given the middleware generalError", () => {
+  describe("When it receives a error with a message 'Error', public message 'Error try again' with status 502'", () => {
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    test("Then it should return a response with status 500 with a public message Error try again", () => {
+      const { publicMessage, status } = errorsMessage.defaultGeneralError;
+
+      const expectedPublicMessage = publicMessage;
+      const expectedStatus = 500;
+
+      const error = new CustomError("Error", expectedPublicMessage, status);
+
+      generalError(error, null, res as Response, null);
+
+      expect(res.json).toBeCalledWith({ error: expectedPublicMessage });
+      expect(res.status).toBeCalledWith(expectedStatus);
+    });
+  });
+  describe("When it receives a error without a message, public message 'Error try again' with status 0", () => {
+    const res: Partial<Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    test("Then it should return a response with status 500 with a public message 'Fatal error'", () => {
+      const { publicMessage: message, status } =
+        errorsMessage.defaultGeneralError;
+
+      const expectedPublicMessage = message;
+      const expectedStatus = status;
+
+      const error = new CustomError("Error", undefined, null);
+
+      generalError(error, null, res as Response, null);
+
+      expect(res.json).toBeCalledWith({ error: expectedPublicMessage });
       expect(res.status).toBeCalledWith(expectedStatus);
     });
   });
