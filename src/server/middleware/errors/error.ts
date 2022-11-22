c99e5a00ecde05845efd2a6eccfe3616d6f54863
Adria <adriamartinez94@gmail.com>
2022-11-22 12:09:29 +0100

    Implement validation error on generalError middleware

--- src/server/middleware/errors/error.ts
+++ src/server/middleware/errors/error.ts
@@ -4,6 +4,7 @@ import createDebug from "debug";
 import chalk from "chalk";
 import type CustomError from "../../../CustomError/CustomError.js";
 import errorsMessage from "../../../errorsMessage.js";
+import { ValidationError } from "express-validation";
 
 const debug = createDebug(`${environment.debug}server`);
 
@@ -20,6 +21,18 @@ export const generalError = (
   // eslint-disable-next-line no-unused-vars
   next: NextFunction
 ) => {
+  if (error instanceof ValidationError) {
+    debug(
+      chalk.red.bold(
+        (error as ValidationError).details.body
+          .map((error) => error.message)
+          .join("\n")
+      )
+    );
+    error.publicMessage = errorsMessage.validationError.publicMessage;
+    error.statusCode = errorsMessage.validationError.statusCode;
+  }
+
   const { publicMessage: message, status } = errorsMessage.defaultGeneralError;
 
   const statusCode = error.statusCode ?? status;
