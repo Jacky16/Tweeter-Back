6c1bdb161a3f969fe909e64dd3e9fff847d503f1
Adria <adriamartinez94@gmail.com>
2022-11-22 13:34:36 +0100

    Add test of registerUser controller

---
+++ src/server/controllers/userControllers/userController.test.ts
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
+/* eslint-disable @typescript-eslint/await-thenable */
+
+import type { Request, Response } from "express";
+import User from "../../../database/models/User.js";
+import {
+  getRandomUserBd,
+  getRandomUserRegisterCredentials,
+} from "../../../factories/usersFactory.js";
+import bcrypt from "bcrypt";
+import { registerUser } from "./userControllers.js";
+import type { UserBd as UserDb } from "../../types.js";
+import errorsMessage from "../../../errorsMessage.js";
+
+describe("Given the registerUser controller", () => {
+  const userCredentials = getRandomUserRegisterCredentials({
+    username: "Mario",
+    email: "mario@gmail.com",
+    password: "123",
+  })();
+  const req: Partial<Request> = {
+    body: userCredentials,
+  };
+
+  const res: Partial<Response> = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  describe("When receives a request with a body username mario and password 123", () => {
+    const userDatabase: Partial<UserDb> = {
+      username: "Mario",
+      email: "mario@gmail.com",
+      password: "hashedPassword",
+    };
+    const userDb = getRandomUserBd(userDatabase as UserDb)();
+    const hashedPassword = "abc";
+
+    test("Then it should return a response with status 201 with the mario username and his email", async () => {
+      const expectedStatus = 201;
+      const expectedUser = {
+        user: {
+          id: userDb._id.toString(),
+          username: userDb.username,
+          email: userDb.email,
+        },
+      };
+
+      User.create = jest.fn().mockReturnValueOnce(userDb);
+      bcrypt.hash = jest.fn().mockReturnValue(hashedPassword);
+
+      await registerUser(req as Request, res as Response, null);
+
+      expect(res.status).toBeCalledWith(expectedStatus);
+      expect(res.json).toBeCalledWith(expectedUser);
+    });
+  });
+  describe("When try to create a existing user", () => {
+    test("Then next should be called with error 'User exist'", async () => {
+      const next = jest.fn();
+
+      const errorExpected = errorsMessage.registerUser;
+
+      User.create = jest.fn().mockRejectedValue(errorExpected);
+
+      await registerUser(req as Request, res as Response, next);
+
+      expect(next).toBeCalledWith(errorExpected);
+    });
+  });
+});
--- src/server/controllers/userControllers/userControllers.ts
+++ src/server/controllers/userControllers/userControllers.ts
@@ -8,14 +8,18 @@ import bcrypt from "bcrypt";
 export const registerUser: RequestHandler = async (req, res, next) => {
   try {
     const { username, password, email } = req.body as UserRegisterCredentials;
+
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = await User.create({
       username,
       password: hashedPassword,
       email,
     });
 
-    res.status(201).json({ user: { username, email: newUser.email } });
+    res.status(201).json({
+      user: { id: newUser._id.toString(), username, email: newUser.email },
+    });
   } catch (error: unknown) {
     const customError = new CustomError(
       (error as Error).message,
