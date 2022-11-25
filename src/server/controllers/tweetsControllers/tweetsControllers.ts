ea4b939f4f9470eac5331c859bd544cdd4e0069e
Adria <adriamartinez94@gmail.com>
2022-11-25 12:39:46 +0100

    Add getTweets controller

---
+++ src/server/controllers/tweetsControllers/tweetsControllers.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-implicit-coercion */
+import type { RequestHandler } from "express";
+import type { Error } from "mongoose";
+import Tweet from "../../../database/models/Tweet.js";
+import errorsMessage from "../../../errorsMessage.js";
+
+export const getTweets: RequestHandler = async (req, res, next) => {
+  const { page = 1, limit = 10 } = req.query;
+
+  try {
+    const currentPage = +page;
+
+    const totalPages = Math.ceil((await Tweet.count()) / +limit);
+
+    if (currentPage < 1 || currentPage > totalPages) {
+      next(errorsMessage.tweets.paginationRangeError);
+      return;
+    }
+
+    const tweets = await Tweet.find()
+      .limit(+limit)
+      .skip((+page - 1) * +limit)
+      .exec();
+
+    if (!tweets || tweets.length === 0) {
+      next(errorsMessage.tweets.tweetsNotfound);
+    }
+
+    res.status(200).json({
+      totalPages,
+      currentPage: +page,
+      tweets,
+    });
+  } catch (error: unknown) {
+    next(error as Error);
+  }
+};
