58be788aaa37c5bafd1ee613caa74090d733ec1e
Adria <adriamartinez94@gmail.com>
2022-11-25 20:54:51 +0100

    Fix user factories

--- src/factories/usersFactory.ts
+++ src/factories/usersFactory.ts
@@ -5,41 +5,39 @@ import type {
   UserLoginCredentials,
   UserRegisterCredentials,
 } from "../server/types";
-import mongoose from "mongoose";
 
-const usersRegisterCredentialsFactory = (user?: UserRegisterCredentials) =>
-  Factory.define<UserRegisterCredentials>(() => ({
-    username: user?.username ?? faker.internet.userName(),
-    password: user?.password ?? faker.internet.password(),
-    email: user?.email ?? faker.internet.email(),
-    alias: user?.alias ?? `@${faker.name.lastName()}`,
-  }));
+const usersRegisterCredentialsFactory = Factory.define<UserRegisterCredentials>(
+  () => ({
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    email: faker.internet.email(),
+    alias: faker.name.lastName(),
+  })
+);
 
-const usersDbFactory = (user?: UserBd) =>
-  Factory.define<UserBd>(() => ({
-    _id: user?._id ?? new mongoose.Types.ObjectId(),
-    username: user?.username ?? faker.internet.userName(),
-    password: user?.password ?? faker.internet.password(),
-    email: user?.email ?? faker.internet.email(),
-    alias: user?.alias ?? `@${faker.name.lastName()}`,
-  }));
+const usersDbFactory = Factory.define<UserBd>(() => ({
+  _id: faker.database.mongodbObjectId(),
+  username: faker.internet.userName(),
+  password: faker.internet.password(),
+  email: faker.internet.email(),
+  alias: faker.name.lastName(),
+}));
 
-const usersLoginCredentialsFactory = (user?: UserLoginCredentials) =>
-  Factory.define<UserLoginCredentials>(() => ({
-    password: user?.password ?? faker.internet.password(),
-    email: user?.email ?? faker.internet.email(),
-  }));
+const usersLoginCredentialsFactory = Factory.define<UserLoginCredentials>(
+  () => ({
+    password: faker.internet.password(),
+    email: faker.internet.email(),
+  })
+);
 
-export const getRandomUserBd = (user?: UserBd) => () =>
-  usersDbFactory(user).build();
+export const getRandomUserBd = (user?: UserBd) => usersDbFactory.build(user);
 
-export const getRandomUserList = (listUser: number) => () =>
-  usersDbFactory().buildList(listUser);
+export const getRandomUserList = (listUser: number) =>
+  usersDbFactory.buildList(listUser);
 
-export const getRandomUserRegisterCredentials =
-  (user?: UserRegisterCredentials) => () =>
-    usersRegisterCredentialsFactory(user).build();
+export const getRandomUserRegisterCredentials = (
+  user?: UserRegisterCredentials
+) => usersRegisterCredentialsFactory.build(user);
 
-export const getRandomUserLoginCredentials =
-  (user?: UserLoginCredentials) => () =>
-    usersLoginCredentialsFactory(user).build();
+export const getRandomUserLoginCredentials = (user?: UserLoginCredentials) =>
+  usersLoginCredentialsFactory.build(user);
