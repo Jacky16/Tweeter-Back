f5598513c618d009bfa26303dbe2ca402c6afcdf
Adria <adriamartinez94@gmail.com>
2022-11-30 09:34:05 +0100

    Add environment vars of supabase on loadEnvironment

--- src/loadEnvironments.ts
+++ src/loadEnvironments.ts
@@ -8,6 +8,8 @@ const environment = {
   mongoDbUrl: process.env.MONGODB_URL,
   mongoDbDebug: process.env.MONGODB_DEBUG === "true",
   jwtSecret: process.env.JWT_SECRET,
+  supabaseUrl: process.env.SUPABASE_URL,
+  supabaseKey: process.env.SUPABASE_KEY,
 };
 
 export default environment;
