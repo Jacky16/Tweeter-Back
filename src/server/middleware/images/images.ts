1d0a338ee2fa4b00f98c52609261f999f135855b
Adria <adriamartinez94@gmail.com>
2022-12-02 20:23:09 +0100

    Create middleware of getTweetImage

--- src/server/middleware/images/images.ts
+++ src/server/middleware/images/images.ts
@@ -1,41 +1,24 @@
-import type { NextFunction, Response } from "express";
-import Tweet from "../../../database/models/Tweet.js";
-import type { ImageRequest } from "../../types";
-import path from "path";
-import fs from "fs";
+import type { NextFunction, Response, Request } from "express";
 import { bucket } from "../../../utils/supabase.js";
 
-export const getImage = async (
-  req: ImageRequest,
+export const getTweetImage = async (
+  req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const { idTweet } = req.params;
+  const { originalUrl } = req;
 
-    const { image: imageName } = await Tweet.findById(idTweet)
-      .select("image")
-      .exec();
-
-    const existsImageInLocal = fs.existsSync(
-      path.join("assets", "images", imageName)
-    );
-
-    if (!existsImageInLocal) {
-      const {
-        data: { publicUrl },
-      } = bucket.getPublicUrl(imageName);
-      req.publicImageUrl = publicUrl;
-
-      next();
-      return;
-    }
-
-    req.publicImageUrl = `${req.protocol}://${req.get(
-      "host"
-    )}/assets/images/${imageName}`;
+  if (!originalUrl.startsWith("/assets")) {
     next();
-  } catch (error: unknown) {
-    next(error as Error);
+    return;
   }
+
+  const [, , , imageName] = originalUrl.split("/");
+
+  const {
+    data: { publicUrl },
+  } = bucket.getPublicUrl(imageName);
+
+  res.redirect(publicUrl);
+  next();
 };
