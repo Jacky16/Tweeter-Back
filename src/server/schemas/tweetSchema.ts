4a2b8e71b5461005fe370ffdb5d5e8cc05ec1ae4
Adria <adriamartinez94@gmail.com>
2022-12-02 15:38:26 +0100

    Add max 280 chars con description property of tweetSchema

--- src/server/schemas/tweetSchema.ts
+++ src/server/schemas/tweetSchema.ts
@@ -3,10 +3,11 @@ import { Joi } from "express-validation";
 const tweetSchema = {
   body: Joi.object({
     author: Joi.string().required(),
-    category: Joi.string().valid("comedy", "sports").required(),
+    category: Joi.string()
+      .valid("comedy", "sports", "science", "entertainment", "political")
+      .required(),
     visibilityOpen: Joi.boolean().required(),
-    description: Joi.string().required(),
-    dateOfCreation: Joi.date().required(),
+    description: Joi.string().required().max(280),
   }),
 };
 
