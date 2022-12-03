093ebd514b86ba09bbb1e16ec9da910fbfb0dc25
Adria <adriamartinez94@gmail.com>
2022-12-03 14:11:06 +0100

    Implement array of categories on tweetSchema

--- src/server/schemas/tweetSchema.ts
+++ src/server/schemas/tweetSchema.ts
@@ -1,10 +1,11 @@
 import { Joi } from "express-validation";
+import categories from "../../categories.js";
 
 const tweetSchema = {
   body: Joi.object({
     author: Joi.string().required(),
     category: Joi.string()
-      .valid("comedy", "sports", "science", "entertainment", "political")
+      .valid(...categories)
       .required(),
     visibilityOpen: Joi.boolean().required(),
     description: Joi.string().required().max(280),
