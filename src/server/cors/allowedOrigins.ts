00989840b31dd90996da0110d4b2362c04aec5a1
Adria Martinez <adriamartinez94@gmail.com>
2022-12-19 18:30:06 +0100

    Add new origin on allowed origins

--- src/server/cors/allowedOrigins.ts
+++ src/server/cors/allowedOrigins.ts
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:4000",
   "https://adria-martin-final-project-202209-bcn.netlify.app",
   "http://localhost:3000",
+  "https://tweeter-back.onrender.com",
 ];
 
 export default allowedOrigins;
